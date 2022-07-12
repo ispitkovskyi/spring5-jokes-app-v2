88cd7fa322115da99bb9377cc5ed43dd887fa4c3
ispitkovskyi <spitkovskyigor@gmail.com>
2022-07-12 22:30:27 +0300

    created spring MVC controller

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by igors on 7/12/22
+ */
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    //@Autowired - spring will automatically do that, when there's only 1 constructor
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""}) //any of these strings in URL should cause this method execution
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
